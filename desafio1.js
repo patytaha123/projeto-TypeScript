bbb4d8e677d1ecfd11405ed274fb6e69cc9e3475
PATY <patytaha123@gmail.com>
2022-08-06 12:03:46 -0300

    projeto DIO Typescript

---
+++ desafio1.js
@@ -0,0 +1,22 @@
+
+let pessoa1 = {};
+pessoa1.nome = "Telma";
+pessoa1.idade = 33;
+pessoa1.profissao = "Professora"
+
+let pessoa2 = {}
+pessoa2.nome = "João";
+pessoa2.idade = 355;
+pessoa2.profissao = "Pedreiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome :"carlos",
+    idade :19,
+    profissao :"padeiro"
+}
